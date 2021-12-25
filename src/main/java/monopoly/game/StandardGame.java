c9c18e4fa19cfab5591b16a76faddd9bc5e2a68c
sundar <sundararajan.s87@gmail.com>
2021-12-25 15:31:46 -0800

    Renamed Game to StandardGame and NewGameTest to NewStandardGameTest. Added some functionality to assert player name sizes.

---
+++ src/main/java/monopoly/game/StandardGame.java
@@ -0,0 +1,46 @@
+package monopoly.game;
+
+import monopoly.board.Board;
+import monopoly.dice.Dice;
+import monopoly.init.StandardBoardMaker;
+import monopoly.player.Player;
+import monopoly.turn.Move;
+import monopoly.turn.Option;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StandardGame {
+    private final Map<String, Player> namedPlayers;
+
+    public StandardGame(String ...names) throws Exception {
+        if(names.length < 2 || names.length > 4) {
+            throw new Exception();
+        }
+
+        Board board = new StandardBoardMaker().makeBoard();
+        Dice dice = new Dice();
+        Map<String, Player> namedPlayers = new LinkedHashMap<>();
+        for(String name : names) {
+            if(name.equals("")) {
+                throw new Exception();
+            }
+
+            Player player = new Player(board, dice);
+            player.addOption(namedPlayers.isEmpty() ? Move.TURN_TO_PLAY : Move.CANNOT_PLAY, new Option());
+            namedPlayers.put(name, player);
+        }
+
+        this.namedPlayers = namedPlayers;
+    }
+
+    public List<Player> players() {
+        return new ArrayList<>(namedPlayers.values());
+    }
+
+    public Map<String, Player> namedPlayers() {
+        return namedPlayers;
+    }
+}
---
+++ src/test/java/monopoly/game/NewStandardGameTest.java
@@ -0,0 +1,90 @@
+package monopoly.game;
+
+import monopoly.player.Player;
+import monopoly.turn.Move;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewStandardGameTest {
+    @Test
+    public void gameAcceptsPlayerNames() throws Exception {
+        new StandardGame("Player1", "Player2");
+    }
+
+    @Test
+    public void onlyFirstPlayerCanPlayAfterNewGameCreated() throws Exception {
+        // Given
+        StandardGame game = new StandardGame("Beep", "Bop", "Boop", "Burp");
+
+        // When
+        List<Player> players = game.players();
+
+        // Then
+        // Player 1 can play.
+        assertTrue(players.get(0).hasOptionTo(Move.TURN_TO_PLAY));
+        assertFalse(players.get(0).hasOptionTo(Move.CANNOT_PLAY));
+
+        // Player 2 cannot play.
+        assertFalse(players.get(1).hasOptionTo(Move.TURN_TO_PLAY));
+        assertTrue(players.get(1).hasOptionTo(Move.CANNOT_PLAY));
+
+        // Player 3 cannot play.
+        assertFalse(players.get(2).hasOptionTo(Move.TURN_TO_PLAY));
+        assertTrue(players.get(2).hasOptionTo(Move.CANNOT_PLAY));
+
+        // Player 4 cannot play.
+        assertFalse(players.get(3).hasOptionTo(Move.TURN_TO_PLAY));
+        assertTrue(players.get(3).hasOptionTo(Move.CANNOT_PLAY));
+    }
+
+    @Test
+    public void gameCreatesPlayersOnceAndOnlyOnce() throws Exception {
+        // Given
+        StandardGame game = new StandardGame("Beep", "Bop", "Boop");
+
+        // When
+        List<Player> players1 = game.players();
+        List<Player> players2 = game.players();
+
+        // Then
+        assertEquals(players1, players2);
+    }
+
+    @Test
+    public void gameAssociatesNamesWithPlayers() throws Exception {
+        // Given
+        StandardGame game = new StandardGame("Beep", "Bop", "Boop");
+
+        // When
+        Map<String, Player> namedPlayers = game.namedPlayers();
+        assertEquals(3, namedPlayers.size());
+        assertNotNull(namedPlayers.get("Beep"));
+        assertNotNull(namedPlayers.get("Boop"));
+        assertNotNull(namedPlayers.get("Bop"));
+        assertNull(namedPlayers.get("Random1"));
+    }
+
+    @Test
+    public void gameDoesntAcceptLessThen2Players() {
+        assertThrows(Exception.class, () -> new StandardGame("Bleep"));
+    }
+
+    @Test
+    public void gameDoesntAcceptMoreThan4Players() {
+        assertThrows(Exception.class, () -> new StandardGame("Beep", "Bop", "Boop", "Burp", "Zorg"));
+    }
+
+    @Test
+    public void playerNamesShouldHaveAtleastOneCharacter() {
+        assertThrows(Exception.class, () -> new StandardGame("", "lol", "123"));
+    }
+
+    @Test
+    public void playerNamesShouldNotBeNull() {
+        assertThrows(Exception.class, () -> new StandardGame("bleep", "bloop", null));
+    }
+}
