9598d8002110e90d1cf647b9142f9646ebab52d8
sundar <sundararajan.s87@gmail.com>
2021-12-25 21:33:19 -0800

    1. Cannot start a game with two players having the same name. 2. Renamed GameServiceTest to GameServiceNewGameStartTest.

--- src/main/java/monopoly/game/StartingPlayersNames.java
+++ src/main/java/monopoly/game/StartingPlayersNames.java
@@ -1,17 +1,26 @@
 package monopoly.game;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class StartingPlayersNames {
     private final List<String> names;
 
     public StartingPlayersNames(List<String> names) throws Exception {
+        Set<String> seenNames = new HashSet<>();
         if(names.size() < 2 || names.size() > 4) {
             throw new Exception();
         }
         for(String name : names) {
             if (name.equals("")) {
                 throw new Exception();
+            }
+
+            if(seenNames.contains(name)) {
+                throw new Exception("Two players cannot have the same name");
+            } else {
+                seenNames.add(name);
             }
         }
 
--- src/test/java/monopoly/ports/in/GameServiceNewGameStartTest.java
+++ src/test/java/monopoly/ports/in/GameServiceNewGameStartTest.java
@@ -2,39 +2,63 @@ package monopoly.ports.in;
 
 import monopoly.ports.out.EventNotifier;
 import monopoly.ports.out.EventNotifierTestDouble;
-import monopoly.ports.out.NotificationEvent;
 import monopoly.turn.Move;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class GameServiceTest {
+public class GameServiceNewGameStartTest {
 
     @Test
     public void startGameWithSomePlayersTest() throws Exception {
+        // Given
         GameService gameService = new StandardGameService(new EventNotifierTestDouble(new ArrayList<>()));
         gameService.addPlayer("Zip");
         gameService.addPlayer("Zop");
         gameService.addPlayer("Zap");
+
+        // When
         gameService.startGame();
+
+        // Then
+        // Nothing expected to happen.
     }
 
     @Test
     public void startingANewGameNotifiesFirstPlayerItsTheirTurnToPlay() throws Exception {
+        // Given
         List<EventNotifierTestDouble.PlayerEvent> playerEvents = new ArrayList<>();
         EventNotifier eventNotifier = new EventNotifierTestDouble(playerEvents);
         GameService gameService = new StandardGameService(eventNotifier);
         gameService.addPlayer("bleep");
         gameService.addPlayer("bloop");
+
+        // When
         gameService.startGame();
 
+        // Then
         assertEquals("bleep", playerEvents.get(0).name);
         assertEquals(Move.TURN_TO_PLAY, playerEvents.get(0).move);
 
         assertEquals("bloop", playerEvents.get(1).name);
         assertEquals(Move.CANNOT_PLAY, playerEvents.get(1).move);
+    }
+
+    @Test
+    public void startingGameWithDuplicatePlayersThrowsException() {
+        // Given
+        List<EventNotifierTestDouble.PlayerEvent> playerEvents = new ArrayList<>();
+        EventNotifier eventNotifier = new EventNotifierTestDouble(playerEvents);
+        GameService gameService = new StandardGameService(eventNotifier);
+        gameService.addPlayer("Goo");
+        gameService.addPlayer("Gaa");
+        gameService.addPlayer("Goo");
+
+        // When && Then
+        Exception thrownException = assertThrows(Exception.class, gameService::startGame);
+        assertTrue(thrownException.getMessage().contains("Two players cannot have the same name"));
     }
 }
