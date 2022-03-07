bc404724f47497e8916834b817b211b7e4509377
sundar <sundararajan.s87@gmail.com>
2022-03-07 00:07:07 -0800

    Refactored Players for readability.

--- src/main/java/monopoly/player/Players.java
+++ src/main/java/monopoly/player/Players.java
@@ -31,6 +31,17 @@ public class Players {
 
     public void endTurn() {
         players.get(currentPlayersIndex).endTurn();
+        int newPlayersIndex = findNextActivePlayersIndex();
+        players.get(newPlayersIndex).addOption(Move.TURN_TO_PLAY, new Option());
+        currentPlayersIndex = newPlayersIndex;
+    }
+
+    public void quitGameFor(String playerName) {
+        nameToPlayerMap().get(playerName).quit();
+        notifyWinnerIfOnlyOnePlayerIsStillActive();
+    }
+
+    private int findNextActivePlayersIndex() {
         int newPlayersIndex = currentPlayersIndex + 1;
         if (newPlayersIndex >= players.size()) {
             newPlayersIndex = 0;
@@ -43,12 +54,15 @@ public class Players {
             }
         }
 
-        players.get(newPlayersIndex).addOption(Move.TURN_TO_PLAY, new Option());
-        currentPlayersIndex = newPlayersIndex;
+        return newPlayersIndex;
     }
 
-    public void quitGameFor(String playerName) {
-        nameToPlayerMap().get(playerName).quit();
+    private void notifyWinnerIfOnlyOnePlayerIsStillActive() {
+        if (moreThanOnePlayerIsActive()) return;
+        notifyActivePlayerTheyHaveWon();
+    }
+
+    private boolean moreThanOnePlayerIsActive() {
         int numberOfPlayersWhoHaveQuit = 0;
         for(Player player : players) {
             if(player.hasQuit()) {
@@ -56,12 +70,14 @@ public class Players {
             }
         }
 
-        if(players.size() - numberOfPlayersWhoHaveQuit == 1) {
-            for(Player player : players) {
-                if(!player.hasQuit()) {
-                    player.addOption(Move.WON, new Option());
-                    break;
-                }
+        return players.size() - numberOfPlayersWhoHaveQuit > 1;
+    }
+
+    private void notifyActivePlayerTheyHaveWon() {
+        for(Player player : players) {
+            if(!player.hasQuit()) {
+                player.addOption(Move.WON, new Option());
+                break;
             }
         }
     }
